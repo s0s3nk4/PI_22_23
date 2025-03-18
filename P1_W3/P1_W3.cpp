7ce4cc0669d5a8bc6a4d3deedc279be4fb903e82
s0s3nk4 <msosna2929@gmail.com>
2025-03-18 16:59:39 +0100

    Dodaj pliki projektów.

---
+++ P1_W3/P1_W3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	// Rzutowanie jawne
+	double x = 10.5;
+	int y = (int)x; // y = 10
+
+	// Rzutowanie C++
+	double x = 10.5;
+	int y = static_cast<int>(x); // y = 10
+
+	// Rzutowanie wymuszone
+	int a = 65;
+	char b = reinterpret_cast<char&>(a); // b = 'A'
+
+	// Wyrażenie warunkowe - działa jak skrócony if-else
+	int a = 5, b = 10;
+	int min = (a < b) ? a : b; // min = 5
+
+	// Operator przecinka - kilka operacji w jednej instrukcji
+	int x, y;
+	x = (y = 5, y + 10); // y = 5, potem x = y + 10 -> x = 15
+}
---
+++ PI_W2/PI_W2.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+using namespace std;
+
+int liczba = 0;
+double pi = 3.14159265358979323846;
+char litera = 'A';
+bool prawda = true;
+
+int x; // Deklaracja (przydzielenie pamięci, bez określenia wartości)
+int y = 5; // Inicjalizacja (przydzielenie pamięci i określenie wartości)
+
+const double PI = 3.14159265358979323846; 
+#define MAX 100 
+
+int liczba2 = 10;
+
+char znak = 'B';
+
+int main()
+{
+	int wiek;
+	cout << "Podaj swoj wiek: ";
+	cin >> wiek;
+	cout << "Twoj wiek to: " << wiek << endl;
+
+	if (liczba2 > 0)
+	{
+		cout << "Liczba jest dodatnia" << endl;
+	}
+	else if (liczba2 < 0)
+	{
+		cout << "Liczba jest ujemna" << endl;
+	}
+	else
+	{
+		cout << "Liczba jest rowna zero" << endl;
+	}
+
+	switch (znak)
+	{
+	case 'A':
+		cout << "Znak to A" << endl;
+		break;
+	case 'B':
+		cout << "Znak to B" << endl;
+		break;
+	case 'C':
+		cout << "Znak to C" << endl;
+		break;
+	default:
+		cout << "Nieznany znak" << endl;
+		break;
+	}
+
+	// for
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "Iteracja " << i << endl;
+	}
+
+	// while
+	int x = 0;
+	while (x < 10)
+	{
+		cout << "x = " << x << endl;
+		x++;
+	}
+
+	// do while
+	int y = 0;
+	do
+	{
+		cout << "y = " << y << endl;
+		y++;
+	} while (y < 10);
+
+	if (x > 0)
+	{
+		cout << "Liczba jest dodatnia" << endl;
+		cout << "Instrukcja złożona" << endl;
+	}
+
+	return 0;
+}
+
